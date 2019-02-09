e109c7b90ce3368f232f3ac6fb4896a4598bc347
David Rivera <davidrivera62@gmail.com>
2019-02-09 09:26:24 -0500

    IRKernel Update

--- analytics.sh
+++ analytics.sh
@@ -1,16 +1,17 @@
 
 #Instalacion Anaconda
 
-bash /home/drivera/Descargas/Anaconda3-5.1.0-Linux-x86_64.sh
+#bash /home/drivera/Descargas/Anaconda3-5.1.0-Linux-x86_64.sh
 
 #export PATH adding Anaconda directory
-export PATH=$PATH:/home/drivera/anaconda3/bin:$PATH
+#export PATH=$PATH:/home/drivera/anaconda3/bin:$PATH
 
 #instalacion librerias
 
-sudo apt install libssl-dev libssh2-1-dev libxml2-dev
-sudo apt install build-essential python-dev default-jre default-jdk
-sudo apt install libcairo2-dev libjpeg62 zlib1g-dev libcurl4-gnutls-dev
+sudo apt-get install -y libzmq3-dev libcurl-openssl1.0-dev
+sudo apt-get install -y libssl-dev libssh2-1-dev libxml2-dev
+sudo apt-get install -y build-essential python-dev default-jre default-jdk
+sudo apt-get install -y libcairo2-dev libjpeg62 zlib1g-dev libcurl4-gnutls-dev
 
 #Add CRAN as repository
 
@@ -23,12 +24,12 @@ gpg -a --export E084DAB9 | sudo apt-key add -
 sudo apt -y update
 
 #R Install
-sudo apt install -y r-base r-base-dev
+sudo apt-get install -y r-base r-base-dev
 
 #set all Java variables
 sudo R CMD javareconf
 
-sudo usermod -a -G staff your_user_name
+sudo usermod -a -G staff drivera
 
 #Run R script IRkernel
 Rscript script-irkernel.R
--- script-irkernel.R
+++ script-irkernel.R
@@ -1,9 +1,10 @@
 ##
 ## instalación https://irkernel.github.io/installation/
 ##
-install.packages(c('repr', 'IRdisplay','evaluate','crayon','pbdZMQ','devtools','uuid','digest'))
+#install.packages(c('repr', 'IRdisplay','evaluate','crayon','pbdZMQ','devtools','uuid','digest'))
+install.packages(c('repr', 'IRdisplay', 'IRkernel'), type = 'source')
 
-devtools::install_github('IRkernel/IRkernel', force='TRUE')
-devtools::install_github('IRkernel/IRdisplay')
-IRkernel::installspec(user=FALSE)
+#devtools::install_github('IRkernel/IRkernel', force='TRUE')
+#devtools::install_github('IRkernel/IRdisplay')
+#IRkernel::installspec(user=FALSE)
 IRkernel::installspec()
