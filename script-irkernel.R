33189667b61eef274823728c725a3b48c45142f2
David Rivera <davidrivera62@gmail.com>
2019-05-31 14:14:29 -0500

    Script update

--- script-irkernel.R
+++ script-irkernel.R
@@ -2,7 +2,12 @@
 ## instalación https://irkernel.github.io/installation/
 ##
 #install.packages(c('repr', 'IRdisplay','evaluate','crayon','pbdZMQ','devtools','uuid','digest'))
-install.packages(c('repr', 'IRdisplay', 'IRkernel'), type = 'source')
+#install.packages(c('repr', 'IRdisplay', 'IRkernel'), type = 'source')
+
+install.packages('repr', type = 'source')
+install.packages('IRdisplay', type = 'source')
+install.packages('IRkernel', type = 'source')
+
 
 #devtools::install_github('IRkernel/IRkernel', force='TRUE')
 #devtools::install_github('IRkernel/IRdisplay')
