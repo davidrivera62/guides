09c8a4e9882e0e8cfe45a1745adc7dc23d950bee
David Rivera <davidrivera62@gmail.com>
2019-08-17 11:13:10 -0500

    Updated libraries

--- Latex.sh
+++ Latex.sh
@@ -1,4 +1,6 @@
 #texstudio repository
 sudo add-apt-repository -y ppa:sunderme/texstudio
 
+sudo apt update
+
 sudo apt-get install -y texlive-full texstudio
--- analytics.sh
+++ analytics.sh
@@ -13,6 +13,8 @@ sudo apt-get install -y libssl-dev libssh2-1-dev libxml2-dev
 sudo apt-get install -y build-essential python-dev default-jre default-jdk
 sudo apt-get install -y libcairo2-dev libjpeg62 zlib1g-dev libcurl4-gnutls-dev
 
+sudo apt-get install -y lib32gcc1 lib32stdc++6 libc6-i386 libclang-6.0-dev libclang-common-6.0-dev libclang-dev libclang1-6.0 libllvm6.0 libobjc-7-dev libobjc4 lib32gcc1 lib32stdc++6 libc6-i386 libclang-6.0-dev libclang-common-6.0-dev libclang-dev libclang1-6.0 libllvm6.0 libobjc-7-dev libobjc4
+
 #Add CRAN as repository
 
 sudo echo "deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/" | sudo tee -a /etc/apt/sources.list
@@ -28,10 +30,11 @@ sudo apt-get install -y r-base r-base-dev
 #set all Java variables
 sudo R CMD javareconf
 
-sudo usermod -a -G staff drivera
+#sudo usermod -a -G staff drivera
 
 #Run R script IRkernel
-Rscript script-irkernel.R
+
+sudo Rscript script-irkernel.R
 
 #install rstudio
 
