98669ff4d7912c6d677ca182f501e947a45b2b6e
David Rivera <davidrivera62@gmail.com>
2020-06-06 12:34:24 -0500

    Update

--- install.sh
+++ install.sh
@@ -59,3 +59,5 @@ sudo apt-get install -y papirus-icon-theme gedit libgnome-keyring0
 
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/zypp/repos.d/vscode.repo'
+
+sudo apt install code
