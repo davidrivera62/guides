4999b69e71132d4be46c1d5e572d59a53db94762
David Fernando Rivera Olarte <davidrivera62@gmail.com>
2022-10-22 08:25:25 -0500

    Update comments

--- vim.sh
+++ vim.sh
@@ -10,4 +10,10 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 
 sudo apt-get install fonts-powerline
 
-
+#vim-fugitive: Already installed   |~                                      
+#vim-monokai: Already installed    |~                                      
+#ale: Already installed            |~                                      
+#vim-airline: Already installed    |~                                      
+#vim-airline-themes: Already instal|~                                      
+#nerdtree: Already installed       |~                                      
+#vim-colorschemes: Already installe
--- zsh.sh
+++ zsh.sh
@@ -40,4 +40,5 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-
+#Font
+#mseloLGS NF font
